aa9967783d850564ad319be5d6715b66f43112e2
Afonso Alcantara <alcantara.afonso@gmail.com>
2024-08-07 22:07:16 -0300

    front end

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,15 +1,14 @@
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 
 from dotenv import load_dotenv
-import os
+from typing import Tuple
 
-def ice_breaker_with(name: str) -> str:
+def ice_breaker_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
@@ -21,6 +20,7 @@ def ice_breaker_with(name: str) -> str:
         \n{format_instructions}
         """
 
+    # parses são usados para formatar as instruções para o LLM
     # summary_prompt_template = PromptTemplate(input_variables=["information"], 
     #     template=summary_template,
     #     partial_variables={"format_instructions": "Por favor, siga o formato: \n\nResumo: \nFato 1: \nFato 2: \n\n"})
@@ -28,11 +28,14 @@ def ice_breaker_with(name: str) -> str:
         template=summary_template,
         partial_variables={"format_instructions": summary_parser.get_format_instructions()})
     
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")(llm=llm, prompt=summary_prompt_template)
+    # a linha de código abaixo se lê da seguinte forma:
+    # "summary_prompt_template" é a entrada para o ChatOpenAI (LLM), que é a entrada para o summary_parser
+    # isso é um Syntax Sugar
+    chain = summary_prompt_template | llm | summary_parser
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-
-    res = chain.invoke(input={"information": linkedin_data})
+    res: Summary = chain.invoke(input={"information": linkedin_data})
+    return res, linkedin_data.get("profile_pic_url")
 
 if __name__ == '__main__':
     load_dotenv()
